bc4c2b2d9f0704cf3b653ac9e14a24e80944f039
raphaelagil <raphaela.gil@yahoo.com>
2024-01-05 18:22:06 +0000

    changed the textarea calling the css

---
+++ starter/assets/script.js
@@ -0,0 +1,58 @@
+
+//Show the currently day
+const date = dayjs().format("dddd, D MMMM YYYY");
+const currentTime = dayjs().format('H');
+const time = parseInt(currentTime); // Convert the current hour to an integer for comparison
+console.log(time)
+$("#currentDay").text(date);
+
+
+//Color-code each time block based on past, present, and future
+function generateSequentialTime() {
+  const timeBlocks = $('.time-block');
+
+  timeBlocks.each(function(index) {
+    const hour = index + 8; // Incrementing from 8 AM onwards
+    let period = 'AM';
+
+    if (hour >= 12) {
+      period = 'PM';
+    }
+
+    let formattedHour = hour > 12 ? hour - 12 : hour;
+    const formattedTime = `${formattedHour.toString().padStart(2, '0')} ${period}`;
+   
+    $(this).text(formattedTime);
+
+    const textContent = $(this).next().find('.textarea'); 
+    if (hour < time) {
+      textContent.addClass('past'); 
+    } else if (hour === time) {
+      textContent.addClass('present'); 
+    } else {
+      textContent.addClass('future'); 
+    }
+  });
+}
+generateSequentialTime();
+
+//Retrive the data and print it in the text area
+$(document).ready(function() {
+  $('.textarea').each(function(index) {
+    const savedText = localStorage.getItem(`textareaContent_${index}`);
+    if (savedText) {
+      $(this).val(savedText);
+    }
+  });
+
+  $('.saveBtn').on('click', function(e) {
+    e.preventDefault();
+
+    const index = $('.saveBtn').index(this);
+    const message = $('.textarea').eq(index).val();
+
+    localStorage.setItem(`textareaContent_${index}`, message);
+  });
+});
+
+
